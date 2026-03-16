4fa06386ed70fb67ec0e2eb5ef07ee5d1677e2e1
Adarsh <vadarsh263@gmail.com>
2026-03-16 16:47:34 +0530

    feat: added global error handler in app.js

--- src/app.js
+++ src/app.js
@@ -3,10 +3,14 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import userRouter from './routes/user.routes.js'
+
 
 
 const app = express()
 
+app.use(helmet())
+app.use(morgan('dev'))
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
@@ -16,8 +20,23 @@ app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended:true,limit:'16kb'}))
 app.use(express.static("public"))
 app.use(cookieParser())
-app.use(helmet())
-app.use(morgan('dev'))
+
+
+
+app.use("/api/v1/users",userRouter)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || "Something went wrong"
+
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+        errors: err.errors || [],
+        data: null
+    })
+})
 
 export {app}
 
